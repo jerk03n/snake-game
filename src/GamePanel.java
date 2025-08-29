8853de6e1a60503d17dbfafa8f69c241ec9b3a47
jerk03n <jeremykoen.p04@gmail.com>
2025-08-29 12:09:01 +0800

    snake controller added

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -99,7 +99,11 @@ public class GamePanel extends JPanel implements ActionListener{
     }
 
     public void checkMouse(){
-
+        if(x[0] == mouseX && y[0] == mouseY) {
+			bodyParts++;
+			applesEaten++;
+			newMouse();
+		}
     }
 
     public void checkCollisions(){
@@ -157,7 +161,28 @@ public class GamePanel extends JPanel implements ActionListener{
     public class MyKeyAdapter extends KeyAdapter{
         @Override
         public void keyPressed(KeyEvent e){
-
+            switch(e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if(direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if(direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if(direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if(direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+                }
         }
     }
 }
